8b9e842b57c5952756cc14dcf6cee45d7e834eee
Suhani Sethi <suhanisethi22.2007@gmail.com>
2026-04-08 12:50:14 +0530

    Create day5.c

---
+++ day5.c
@@ -0,0 +1,50 @@
+//A system receives two separate logs of user arrival times from two different servers. Each log is already sorted in ascending order. Your task is to create a single chronological log that preserves the correct order of arrivals.
+#include <stdio.h>
+
+int main() {
+    int p, q;
+
+    // Input size of first log
+    scanf("%d", &p);
+    int a[p];
+
+    // Input first log
+    for(int i = 0; i < p; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    // Input size of second log
+    scanf("%d", &q);
+    int b[q];
+
+    // Input second log
+    for(int i = 0; i < q; i++) {
+        scanf("%d", &b[i]);
+    }
+
+    int i = 0, j = 0;
+
+    // Merge both arrays
+    while(i < p && j < q) {
+        if(a[i] <= b[j]) {
+            printf("%d ", a[i]);
+            i++;
+        } else {
+            printf("%d ", b[j]);
+            j++;
+        }
+    }
+
+    // Print remaining elements
+    while(i < p) {
+        printf("%d ", a[i]);
+        i++;
+    }
+
+    while(j < q) {
+        printf("%d ", b[j]);
+        j++;
+    }
+
+    return 0;
+}
