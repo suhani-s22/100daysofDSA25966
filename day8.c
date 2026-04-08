32af4881220326ceff048b04e3f2cb8112c6412a
Suhani Sethi <suhanisethi22.2007@gmail.com>
2026-04-08 12:50:28 +0530

    Create day8.c

---
+++ day8.c
@@ -0,0 +1,19 @@
+//Given integers a and b, compute a^b using recursion without using pow() function.
+#include <stdio.h>
+
+// Recursive function
+int power(int a, int b) {
+    if (b == 0)
+        return 1;
+
+    return a * power(a, b - 1);
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    printf("%d", power(a, b));
+
+    return 0;
+}
