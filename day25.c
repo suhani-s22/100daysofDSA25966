ae89b4bf34e8c9dee8639fc83b16c41da38e1c07
Suhani Sethi <suhanisethi22.2007@gmail.com>
2026-04-29 15:01:54 +0530

    Create day25.c

---
+++ day25.c
@@ -0,0 +1,61 @@
+// This program counts the number of nodes in a linked list.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Create linked list
+struct Node* createList(int n) {
+    struct Node *head = NULL, *temp = NULL, *newNode;
+
+    for (int i = 0; i < n; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+    return head;
+}
+
+// Count occurrences of key
+int countOccurrences(struct Node* head, int key) {
+    int count = 0;
+    struct Node* temp = head;
+
+    while (temp != NULL) {
+        if (temp->data == key) {
+            count++;
+        }
+        temp = temp->next;
+    }
+
+    return count;
+}
+
+int main() {
+    int n, key;
+
+    scanf("%d", &n);
+    struct Node* head = createList(n);
+
+    scanf("%d", &key);
+
+    int result = countOccurrences(head, key);
+
+    printf("%d", result);
+
+    return 0;
+}
