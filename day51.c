bcebfaafc5b3daf68ef54bb377f0de99eb5dc884
Suhani Sethi <suhanisethi22.2007@gmail.com>
2026-04-29 15:16:30 +0530

    Create day51.c

---
+++ day51.c
@@ -0,0 +1,65 @@
+// This program builds a binary tree from a level order input and performs an inorder traversal.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* newNode(int val) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Insert into BST
+struct Node* insert(struct Node* root, int key) {
+    if (root == NULL)
+        return newNode(key);
+
+    if (key < root->data)
+        root->left = insert(root->left, key);
+    else if (key > root->data)
+        root->right = insert(root->right, key);
+
+    return root;
+}
+
+// Find LCA in BST
+struct Node* findLCA(struct Node* root, int n1, int n2) {
+    if (root == NULL) return NULL;
+
+    if (n1 < root->data && n2 < root->data)
+        return findLCA(root->left, n1, n2);
+
+    if (n1 > root->data && n2 > root->data)
+        return findLCA(root->right, n1, n2);
+
+    return root;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node* root = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    struct Node* lca = findLCA(root, a, b);
+
+    if (lca != NULL)
+        printf("%d", lca->data);
+
+    return 0;
+}
