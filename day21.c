c55d3e03d598e9a2f20545022b17cd1d7d1546bb
Suhani Sethi <suhanisethi22.2007@gmail.com>
2026-04-29 15:01:13 +0530

    Create day21.c

---
+++ day21.c
@@ -0,0 +1,44 @@
+// This program creates a linked list from user input and prints the elements of the list.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *temp = NULL, *newNode;
+
+    // Create linked list
+    for (int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    // Traverse and print
+    temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
