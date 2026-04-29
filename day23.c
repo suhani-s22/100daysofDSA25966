d1c4b6a199e3b2d9b96faacdc70067dcab23218b
Suhani Sethi <suhanisethi22.2007@gmail.com>
2026-04-29 15:01:43 +0530

    Create day23.c

---
+++ day23.c
@@ -0,0 +1,85 @@
+// This program counts the number of nodes in a linked list.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Create linked list
+struct Node* createList(int n) {
+    struct Node *head = NULL, *temp = NULL, *newNode;
+
+    for (int i = 0; i < n; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+    return head;
+}
+
+// Merge two sorted lists
+struct Node* mergeLists(struct Node* l1, struct Node* l2) {
+    struct Node dummy;
+    struct Node* tail = &dummy;
+    dummy.next = NULL;
+
+    while (l1 != NULL && l2 != NULL) {
+        if (l1->data < l2->data) {
+            tail->next = l1;
+            l1 = l1->next;
+        } else {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+        tail = tail->next;
+    }
+
+    // Attach remaining nodes
+    if (l1 != NULL)
+        tail->next = l1;
+    else
+        tail->next = l2;
+
+    return dummy.next;
+}
+
+// Traverse and print list
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main() {
+    int n, m;
+
+    // First list
+    scanf("%d", &n);
+    struct Node* list1 = createList(n);
+
+    // Second list
+    scanf("%d", &m);
+    struct Node* list2 = createList(m);
+
+    // Merge
+    struct Node* merged = mergeLists(list1, list2);
+
+    // Output
+    printList(merged);
+
+    return 0;
+}
